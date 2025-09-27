221e0274d81d1523b25adfdd66887817625a8555
Vuong Nguyen <kudoyaung@gmail.com>
2025-09-27 16:47:29 +0700

    feat(exception): add custom exception and handler with DTO improvements

--- medicine/src/main/java/com/smarthealth/medicine/model/PrescriptionItemDto.java
+++ medicine/src/main/java/com/smarthealth/medicine/model/PrescriptionItemDto.java
@@ -1,9 +1,15 @@
 package com.smarthealth.medicine.model;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PrescriptionItemDto {
 
     @NotNull
