1acdf0ba693d3c976443279b4e7d77ff7db6ed73
Vuong Nguyen <kudoyaung@gmail.com>
2025-10-10 15:45:11 +0700

    feat(config): add @nestjs/config and configure global environment

--- notification/src/app.module.ts
+++ notification/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { NotificationModule } from './modules/notification.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    NotificationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
